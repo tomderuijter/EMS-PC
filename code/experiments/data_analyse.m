b82bb777cc10b02001e6bc6960bd51748e6f96f9
Ramon Janssen <jekanmijvinden@hotmail.com>
2013-11-29 12:11:45 +0100

    data-analyse wat uitgebreid en een colormaps-bestandje toegevoegd (ruzie met colormaps, colormap('mijn_colormap') werkt niet

--- code/experiments/data_analyse.m
+++ code/experiments/data_analyse.m
@@ -1,7 +1,27 @@
-GsPerm = permute(Gs, [1,4,2,3]);
+%load('../../data/multi-subject/mult_subjects_explicit');
 
-GsMean = zeros(77,116,116); 
-for subjNr = 1: 77
-	GsMean(subjNr,:,:) = squeeze(mean(squeeze(GsPerm(subjNr,:,:,:))));
-end
-GsMean = permute(GsMean, [2,3,1]);
+% per subject, only consider connections that appear in at least a fraction
+% of 'perm_treshold' of all permutations.
+perm_treshold = 0.90;
+% in total, only consider connections that appear in at least a fraction
+% of 'subj_treshold' of all subjects.
+subj_treshold = 0.7;
+
+Gs_avg = squeeze(mean(mean(Gs, 4), 1));
+
+% make a filter by applying these tresholds
+filter = squeeze(mean(mean(Gs, 4) > perm_treshold) > subj_treshold);
+filter_per_subject = mean(Gs,4) > perm_treshold;
+
+% just take the avarage of all permutations and subjects and filter this
+PDAG_avg_filtered = ((squeeze(mean(mean(PDAGs, 4),1)) - ones(116,116)) ./ Gs_avg) .* filter;
+PDAG_per_subject_filtered = (mean(PDAGs, 4) - ones(77,116,116)).* filter_per_subject;
+PDAG_assymetric_per_subject = PDAG_per_subject_filtered - permute(PDAG_per_subject_filtered, [1,3,2]);
+%handle = implay(permute(PDAG_assymetric_per_subject, [2,3,1]), 3); % transpose the last dimensions
+%handle.Visual.ColorMap.MapExpression = 'colormap_asymmetric';
+% draw things
+
+figure; imagesc(PDAG_avg_filtered);
+colormap ('colormap_filtered');
+axis square;
+%figure; imagesc((PDAG_avg_filtered - PDAG_avg_filtered')); colormap (colormap_asymmetric); axis square;
